895b89fd3c17600f972a2505f09a9e4dfe622d9f
JhonMedina <jhon-43769@outlook.com>
2024-02-27 18:55:42 -0400

    add get and create controller

--- src/controller/sales.controller.js
+++ src/controller/sales.controller.js
@@ -1,1 +1,35 @@
 import { prisma } from "../db.js";
+
+
+
+export const getSales = async (req, res) => {
+    try {
+      const sales = await prisma.sales.findMany();
+  
+      res.status(201).json(sales);
+    } catch (e) {
+      res.status(400).json({ error: "Error", message: e.message });
+    }
+  };
+
+
+  export const createSale = async (req, res) => {
+    try {
+      const { stock, unitPrice, ...rest } = req.body;
+  
+      const total = stock * unitPrice;
+  
+      const sale = await prisma.sales.create({
+        data: {
+          ...rest,
+          stock,
+          unitPrice,
+          total,
+        },
+      });
+  
+      res.status(201).json(sale);
+    } catch (e) {
+      res.status(400).json({ error: "Error POST method", message: e.message });
+    }
+  };
