e4ae8e98bc7c680ff77aee92233d93c84a9fd75a
JhonMedina <jhon-43769@outlook.com>
2024-02-27 19:00:52 -0400

    addd deletedSale Conctroller

--- src/controller/sales.controller.js
+++ src/controller/sales.controller.js
@@ -1,7 +1,7 @@
 import { prisma } from "../db.js";
 
 
-
+//Get sales Met
 export const getSales = async (req, res) => {
     try {
       const sales = await prisma.sales.findMany();
@@ -13,6 +13,7 @@ export const getSales = async (req, res) => {
   };
 
 
+  //crete sale met
   export const createSale = async (req, res) => {
     try {
       const { stock, unitPrice, ...rest } = req.body;
@@ -30,6 +31,56 @@ export const getSales = async (req, res) => {
   
       res.status(201).json(sale);
     } catch (e) {
-      res.status(400).json({ error: "Error POST method", message: e.message });
+      res.status(400).json({ error: "Error en el metodo", message: e.message });
     }
   };
+
+
+  //update SAlE
+  export const updateSale = async (req, res) => {
+    try {
+      const { id, ...updateData } = req.body;
+  
+      const existingSale = await prisma.sales.findUnique({
+        where: { id },
+      });
+  
+      if (!existingSale) {
+        return res.status(404).json({ error: "Sale not found" });
+      }
+  
+      const updateSale = await prisma.sales.update({
+        where: { id },
+        data: updateData,
+      });
+      res.status(200).json(updateSale);
+    } catch (e) {
+      res.status(400).json({ error: "Error PUT method", message: e.message });
+    }
+  };
+
+
+  //DeleteSale
+  export const deleteSale = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const existingSale = await prisma.sales.findUnique({
+        where: { id },
+      });
+  
+      if (!existingSale) {
+        return res.status(404).json({ error: "Sale not found" });
+      }
+  
+      const deletedSale = await prisma.sales.update({
+        where: { id },
+        data: { deleted: true },
+      });
+  
+      res.status(200).json(deletedSale);
+    } catch (e) {
+      res.status(400).json({ error: "Error DELETE method", message: e.message });
+    }
+  };
+  
